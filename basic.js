04a1f3e83c316cec29f38dbe2950ff17ba2ff287
roshanyacham <roshanyacham301@gmail.com>
2024-05-01 00:56:17 +0530

    first function

--- basic.js
+++ basic.js
@@ -1,8 +1,4 @@
-let name="Roshan";
-let age=30;
-const PI=3.14;
-
-
-console.log("Name:",name)
-console.log("Age:",age)
-console.log("PI",PI)
+function great(name){
+    return 'hello, ' +name+ '!';
+}
+console.log(great("roshan"))
