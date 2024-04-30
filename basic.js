66ef72bc11d582f89727a039d8fd88aae582a46d
roshanyacham <roshanyacham301@gmail.com>
2024-05-01 01:11:48 +0530

    first conditional statement program

--- basic.js
+++ basic.js
@@ -1,4 +1,8 @@
-function great(name){
-    return 'hello, ' +name+ '!';
+let hour=12;
+if(hour<12){
+    console.log('good Morning')
+}else if(hour>=12 && hour<18){
+    console.log("good afternoon")
+}else{
+    console.log("good evening")
 }
-console.log(great("roshan"))
